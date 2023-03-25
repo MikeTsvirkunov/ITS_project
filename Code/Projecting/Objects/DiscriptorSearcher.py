19730baf84d59ebf8574f9a53e6325b091b403fc
Mike Tsvirkunov <mike.tsvirkunov@gmail.com>
2023-03-25 19:26:52 +0600

    Asd

---
+++ Code/Projecting/Objects/DiscriptorSearcher.py
@@ -0,0 +1,12 @@
+from json import load
+import spacy
+
+
+def searchDiscriptorsInSenteces(text):
+    pass
+
+
+if __name__ == '__main__':
+    nlp = spacy.load('ru_core_news_sm')
+    # with open('../../')
+    # searchDiscriptorsInSenteces("")
